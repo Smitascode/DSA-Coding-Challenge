1a0a933ea2976356e26e1c862b320c519f43138e
SMITA <sinhasmita53@gmail.com>
2023-07-04 23:54:02 +0530

    Create Single Number II.cpp

---
+++ Single Number II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+        
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        
+        return ones;
+    //     int ans = 0;
+    //     for(int i=0; i<32;++i){
+    //         int sum = 0;
+    //         for(const int num : nums){
+    //             sum += num >> i & 1;
+    //         }sum %=3;
+    //         ans |= sum << i;
+    //     }
+    //     return ans;
+    }
+};
